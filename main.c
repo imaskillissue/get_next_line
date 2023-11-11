741c9343efe7e15bef2674b91de9698ed0821778
Emil Karpawitz <ekarpawi@c2s3d6.42wolfsburg.de>
2023-11-11 18:06:25 +0100

    current

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
 #include "get_next_line.h"
 
 int main()
 {
-	int fd = open("test.txt", O_RDONLY);
+	int fd = open("1char.txt", O_RDONLY);
 	char *text = get_next_line(fd);
+	printf("%s", text);
+	text = get_next_line(fd);
+	printf("%s", text);
+	text = get_next_line(fd);
 	printf("%s", text);
 	return 0;
 }
--- src/get_next_line.c
+++ src/get_next_line.c
@@ -26,6 +26,8 @@ static char	*read_and_buffer(int fd, char **rtn, char **buffer)
 			free(*buffer);
 			return (NULL);
 		}
+		if (i == 0)
+			break ;
 		*rtn = ft_str_join(*rtn, *buffer);
 		if (ft_strchr(*buffer, '\n'))
 			break ;
@@ -33,14 +35,38 @@ static char	*read_and_buffer(int fd, char **rtn, char **buffer)
 	return (*rtn);
 }
 
+static char *remove_past(char **rtn)
+{
+	char	*new_rtn;
+	int		i;
+	int		j;
+
+	i = 0;
+	if (*rtn == NULL)
+		return (NULL);
+	while ((*rtn)[i] != '\n')
+		i++;
+	new_rtn = ft_calloc(ft_strlen(*rtn) - i + 1, sizeof(char));
+	if (!new_rtn)
+		return (NULL);
+	j = 0;
+	while ((*rtn)[++i])
+		new_rtn[j++] = (*rtn)[i];
+	return (new_rtn);
+}
+
 char	*get_next_line(int fd)
 {
 	static char		*rtn;
 	char			*buffer;
+	int				i;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	rtn = ft_calloc(1, sizeof(char));
+	if (rtn == NULL)
+		rtn = ft_calloc(1, sizeof(char));
+	else
+		rtn = remove_past(&rtn);
 	buffer = ft_calloc(BUFFER_SIZE, sizeof(char));
 	if (!rtn || !buffer)
 		return (NULL);
@@ -48,7 +74,10 @@ char	*get_next_line(int fd)
 	if (!rtn)
 		return (NULL);
 	free(buffer);
-	if (ft_strchr(rtn, '\n'))
-		return (ft_sub_str(rtn, 0, ft_strchr(rtn, '\n') - rtn));
-	return (rtn);
+	if (!ft_strchr(rtn, '\n') || rtn[0] == '\n')
+		return (rtn);
+	i = 0;
+	while (rtn[i++] != '\n')
+		;
+	return (ft_sub_str(rtn, 0, i));
 }
