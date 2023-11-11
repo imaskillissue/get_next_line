9788d24f19be12ec1e7251a30e1608fbc756b68c
Emil Karpawitz <ekarpawi@c2s3d6.42wolfsburg.de>
2023-11-11 16:19:26 +0100

    fixing

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -12,11 +12,31 @@
 
 #include "get_next_line.h"
 
+static char	*read_and_buffer(int fd, char **rtn, char **buffer)
+{
+	int	i;
+
+	i = BUFFER_SIZE;
+	while (i == BUFFER_SIZE)
+	{
+		i = read(fd, *buffer, BUFFER_SIZE);
+		if (i == -1 || (i == 0 && !ft_strlen(*rtn)))
+		{
+			free(*rtn);
+			free(*buffer);
+			return (NULL);
+		}
+		*rtn = ft_str_join(*rtn, *buffer);
+		if (ft_strchr(*buffer, '\n'))
+			break ;
+	}
+	return (*rtn);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*rtn;
-	char		*buffer;
-	int			i;
+	static char		*rtn;
+	char			*buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -24,20 +44,11 @@ char	*get_next_line(int fd)
 	buffer = ft_calloc(BUFFER_SIZE, sizeof(char));
 	if (!rtn || !buffer)
 		return (NULL);
-	i = BUFFER_SIZE;
-	while (i == BUFFER_SIZE)
-	{
-		i = read(fd, buffer, BUFFER_SIZE);
-		if (i == -1 || (i == 0 && !ft_strlen(rtn)))
-		{
-			free(rtn);
-			free(buffer);
-			return (NULL);
-		}
-		rtn = ft_str_join(rtn, buffer);
-		if (ft_strchr(buffer, '\n'))
-			break ;
-	}
+	rtn = read_and_buffer(fd, &rtn, &buffer);
+	if (!rtn)
+		return (NULL);
 	free(buffer);
-	return (ft_sub_str(rtn, 0, (int)(ft_strchr(rtn, '\n') - rtn + 1)));
+	if (ft_strchr(rtn, '\n'))
+		return (ft_sub_str(rtn, 0, ft_strchr(rtn, '\n') - rtn));
+	return (rtn);
 }
