c63889171a16155ece6feb51385b17809ad7c4d4
Emil Karpawitz <ekarpawi@c2s3d6.42wolfsburg.de>
2023-12-15 17:06:33 +0100

    Revert ":)"
    This reverts commit 915c6444fff8b3502a1d0042f19e62a453a488ec.

--- main.c
+++ main.c
@@ -7,11 +7,10 @@ int main()
 {
 	int fd = open("1char.txt", O_RDONLY);
 	char *text = get_next_line(fd);
-	while (text)
-	{
-		printf("%s\n", text);
-		free(text);
-		text = get_next_line(fd);
-	}
+	printf("%s", text);
+	text = get_next_line(fd);
+	printf("%s", text);
+	text = get_next_line(fd);
+	printf("%s", text);
 	return 0;
 }
